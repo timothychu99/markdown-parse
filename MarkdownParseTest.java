50a18207a361d254ac10020db5c058e06e53eeff
timothychu99 <timothychu99@gmail.com>
2022-01-26 16:39:34 -0800

    Markdown_Tester

---
+++ MarkdownParseTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*; //import junit lib
+import org.junit.*; //import junit lib
+
+public class MarkdownParseTest {
+    @Test //tells junit there is start to a test
+    public void addition() { //method to test
+        assertEquals(2, 1 + 1); //see if 1+1 = 2
+    }
+}
